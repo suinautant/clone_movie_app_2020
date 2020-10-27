94bf0f334f10278dc189bd98099b03891a16c93f
suinautant <suinautant@gmail.com>
2020-10-27 20:57:18 +0900

    20201027: axios URL 추가 및 데이터 추출

--- src/App.js
+++ src/App.js
@@ -1,67 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { waitForElementToBeRemoved } from '@testing-library/react';
+import axios from "axios";
+import Movie from "./Movie";
 
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-    "https://www.bobaekimchi.com/shopimages/dudwn0405/002001000001.jpg?1523348965",
-    rating: 5
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-    "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20170910011347_photo1_481363df942d.jpg",
-    rating: 4.9
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-    "https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg",
-    rating: 3.9
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-    "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/guest/image/Q0dyDptUpmvOg_kSyYy0k66j6gY.PNG",
-    rating: 4
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-    "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
-    rating: 4.2
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   }
-];
 
-function Food({name, picture, rating}){
-  return <div>
-    <h1>I Like {name}</h1>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>
-}
+  getMovies = async () => {
+    const { 
+      data: { data: { movies }}
+  } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-  <div>
-    {foodILike.map(dish => (
-    <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />  
-    ))}
-  </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  } 
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading 
+        ? "Loading..." 
+        : movies.map(movie => (
+          <Movie 
+            key={movie.id}
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image} 
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
