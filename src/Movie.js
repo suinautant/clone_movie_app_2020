9a8bb18be689dd9555477821857a96a1c6d1fbee
suinautant <suinautant@gmail.com>
2020-11-02 20:03:53 +0900

    .

--- src/Movie.js
+++ src/Movie.js
@@ -5,16 +5,18 @@ import "./Movie.css";
 function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movies__movie">
-        <img src={poster} alt={title} title={title} />
+      <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
-          <li key={index} className="genres__genre">{genre}</li>
+          <li key={index} className="genres__genre">
+            {genre}
+          </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0,180)}...</p>
       </div>
     </div>
   )
